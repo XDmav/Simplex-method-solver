dac0bf7dbabf2ff62f63add72145a142b7d1c237
XDmav <anonim.ilya.ya@gmail.com>
2025-03-25 21:29:20 +0300

    Const based num of items and update

--- src/main.rs
+++ src/main.rs
@@ -60,7 +60,7 @@ impl Display for Cmp {
 
 #[derive(Clone, Copy)]
 struct Equation {
-	cof: [f64; 3],
+	cof: [f64; NUM_I + 1],
 	cmp: Cmp,
 }
 
@@ -79,26 +79,33 @@ impl Display for Limit {
 	}
 }
 
+const NUM_I: usize = 4;
+const NUM_J: usize = 3;
+
 struct FinalEquation {
-	cof: [f64; 2],
+	cof: [f64; NUM_I],
 	limit: Limit
 }
 
 struct MyApp {
 	final_equation: FinalEquation,
-	equations: [Equation; 3],
-	result: Option<[f64; 2]>,
-	simple_tab_history: Vec<[[f64; 6]; 3]>,
-	basis_history: Vec<[usize; 3]>,
-	delta_history: Vec<[f64; 6]>,
+	equations: [Equation; NUM_J],
+	result: Option<[f64; NUM_I + NUM_J]>,
+	simple_tab_history: Vec<[[f64; NUM_I + NUM_J + 1]; NUM_J]>,
+	basis_history: Vec<[usize; NUM_J]>,
+	delta_history: Vec<[f64; NUM_I + NUM_J + 1]>,
 	dots: Vec<[f64; 2]>,
 }
 
 impl Default for MyApp {
 	fn default() -> Self {
 		Self {
-			final_equation: FinalEquation { cof: [-2.0, 1.0], limit: Limit::Min },
-			equations: [Equation { cof: [1.0, 1.0, 1.0], cmp: Cmp::Gte }; 3],
+			final_equation: FinalEquation { cof: [7.0, 8.0, 6.0, 5.0], limit: Limit::Max },
+			equations: [
+				Equation { cof: [1.0, 3.0, 5.0, 3.0, 40.0], cmp: Cmp::Lte },
+				Equation { cof: [2.0, 6.0, 1.0, 0.0, 50.0], cmp: Cmp::Lte },
+				Equation { cof: [2.0, 3.0, 2.0, 5.0, 30.0], cmp: Cmp::Lte },
+			],
 			result: None,
 			simple_tab_history: Vec::new(),
 			basis_history: Vec::new(),
@@ -116,17 +123,17 @@ fn add_drag_value(ui: &mut Ui, value: &mut f64, label: &str, changed: &mut bool)
 }
 
 impl MyApp {
-	fn change_basis(&mut self, simple_tab: &mut [[f64; 6]; 3], basis: &mut [usize; 3], i: usize, j: usize) {
+	fn change_basis(&mut self, simple_tab: &mut [[f64; NUM_I + NUM_J + 1]; NUM_J], basis: &mut [usize; NUM_J], i: usize, j: usize) {
 		basis[i] = j;
 		let del = simple_tab[i][j];
 		for item in simple_tab[i].iter_mut() {
 			*item /= del;
 		}
 		
-		for p in 0..3 {
+		for p in 0..NUM_J {
 			if p != i {
 				let multi = simple_tab[p][j];
-				for q in 0..6 {
+				for q in 0..NUM_I + NUM_J + 1 {
 					simple_tab[p][q] -= multi * simple_tab[i][q];
 				}
 			}
@@ -136,7 +143,7 @@ impl MyApp {
 		self.basis_history.push(*basis);
 	}
 	
-	fn is_optimal(&self, delta: &[f64; 6]) -> bool {
+	fn is_optimal(&self, delta: &[f64; NUM_I + NUM_J + 1]) -> bool {
 		match self.final_equation.limit {
 			Limit::Max => {
 				if delta.iter().all(|&x| x >= 0.0) {
@@ -197,70 +204,76 @@ impl MyApp {
 	fn update_simple_tab(&mut self) {
 		self.dots = Vec::new();
 		
-		for equation_1 in self.equations {
-			for equation_2 in self.equations {
-				let cord = self.find_intersection(equation_1.cof[0], equation_1.cof[1], equation_1.cof[2],
-				                                  equation_2.cof[0], equation_2.cof[1], equation_2.cof[2]);
-				if let Some(cord) = cord {
+		if NUM_I == 2 {
+			for equation_1 in self.equations {
+				for equation_2 in self.equations {
+					let cord = self.find_intersection(equation_1.cof[0], equation_1.cof[1], equation_1.cof[2],
+					                                  equation_2.cof[0], equation_2.cof[1], equation_2.cof[2]);
+					if let Some(cord) = cord {
+						self.dots.push(cord);
+					}
+				}
+				
+				for equation_2 in [[0.0, 1.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 10000.0], [1.0, 0.0, 10000.0]] {
+					let cord = self.find_intersection(equation_1.cof[0], equation_1.cof[1], equation_1.cof[2],
+					                                  equation_2[0], equation_2[1], equation_2[2]);
+					if let Some(cord) = cord {
+						self.dots.push(cord);
+					}
+				}
+			}
+			
+			for cord in [[0.0, 0.0], [10000.0, 0.0], [10000.0, 10000.0], [0.0, 10000.0]] {
+				if self.if_valid(cord[0], cord[1]) {
 					self.dots.push(cord);
 				}
 			}
 			
-			for equation_2 in [[0.0, 1.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 10000.0], [1.0, 0.0, 10000.0]] {
-				let cord = self.find_intersection(equation_1.cof[0], equation_1.cof[1], equation_1.cof[2],
-				                                  equation_2[0], equation_2[1], equation_2[2]);
-				if let Some(cord) = cord {
-					self.dots.push(cord);
+			let mut centroid_x = 0.0;
+			for dot in self.dots.iter() {
+				centroid_x += dot[0]
+			}
+			centroid_x /= self.dots.len() as f64;
+			
+			let mut centroid_y = 0.0;
+			for dot in self.dots.iter() {
+				centroid_y += dot[1]
+			}
+			centroid_y /= self.dots.len() as f64;
+			
+			self.dots.sort_by(|x1, x2| {
+				if (x1[1] - centroid_y).atan2(x1[0] - centroid_x) < (x2[1] - centroid_y).atan2(x2[0] - centroid_x) {
+					return Ordering::Less
 				}
-			}
+				Ordering::Greater
+			});
 		}
 		
-		for cord in [[0.0, 0.0], [10000.0, 0.0], [10000.0, 10000.0], [0.0, 10000.0]] {
-			if self.if_valid(cord[0], cord[1]) {
-				self.dots.push(cord);
-			}
-		}
-		
-		let mut centroid_x = 0.0;
-		for dot in self.dots.iter() {
-			centroid_x += dot[0]
-		}
-		centroid_x /= self.dots.len() as f64;
-		
-		let mut centroid_y = 0.0;
-		for dot in self.dots.iter() {
-			centroid_y += dot[1]
-		}
-		centroid_y /= self.dots.len() as f64;
-		
-		self.dots.sort_by(|x1, x2| {
-			if (x1[1] - centroid_y).atan2(x1[0] - centroid_x) < (x2[1] - centroid_y).atan2(x2[0] - centroid_x) {
-				return Ordering::Less
-			}
-			Ordering::Greater
-		});
-		
-		let mut simple_tab = [[0.0; 6]; 3];
+		let mut simple_tab = [[0.0; NUM_I + NUM_J + 1]; NUM_J];
 		
 		for (i, tab) in simple_tab.iter_mut().enumerate() {
 			if self.equations[i].cmp == Cmp::Gte {
-				tab[0] = self.equations[i].cof[0] * -1.0;
-				tab[1] = self.equations[i].cof[1] * -1.0;
-				tab[5] = self.equations[i].cof[2] * -1.0;
+				for (j, item_j) in tab.iter_mut().enumerate().take(NUM_I) {
+					*item_j = self.equations[i].cof[j] * -1.0;
+				}
+				tab[NUM_I + NUM_J] = self.equations[i].cof[NUM_I] * -1.0;
 			} else {
-				tab[0] = self.equations[i].cof[0];
-				tab[1] = self.equations[i].cof[1];
-				tab[5] = self.equations[i].cof[2];
+				tab[..NUM_I].copy_from_slice(&self.equations[i].cof[..NUM_I]);
+				tab[NUM_I + NUM_J] = self.equations[i].cof[NUM_I];
 			}
 		}
 		
-		simple_tab[0][2] = 1.0;
-		simple_tab[1][3] = 1.0;
-		simple_tab[2][4] = 1.0;
+		for (i, item_i) in simple_tab.iter_mut().enumerate() {
+			item_i[NUM_I + i] = 1.0;
+		}
 		
 		self.simple_tab_history = vec![simple_tab];
 		
-		let mut basis = [2, 3, 4];
+		let mut basis = [0; NUM_J];
+		
+		for (j, item_j) in basis.iter_mut().enumerate() {
+			*item_j = NUM_I + j
+		}
 		
 		self.basis_history = vec![basis];
 		
@@ -271,8 +284,8 @@ impl MyApp {
 			let mut min_i = None;
 			
 			for (i, tab) in simple_tab.iter().enumerate() {
-				if tab[5] < min {
-					min = tab[5];
+				if tab[NUM_I + NUM_J] < min {
+					min = tab[NUM_I + NUM_J];
 					min_i = Some(i);
 				}
 			}
@@ -282,7 +295,7 @@ impl MyApp {
 				Some(i) => {
 					let mut min = 0.0;
 					let mut min_j = None;
-					for j in 0..5 {
+					for j in 0..NUM_I + NUM_J {
 						if simple_tab[i][j] < min {
 							min = simple_tab[i][j];
 							min_j = Some(j);
@@ -306,35 +319,35 @@ impl MyApp {
 			self.delta_history = Vec::new();
 			
 			for _ in 0..20 {
-				let mut delta = [0.0; 6];
+				let mut delta = [0.0; NUM_I + NUM_J + 1];
 				
 				for (i, item_i) in simple_tab.iter().enumerate() {
 					for (j, item_j) in delta.iter_mut().enumerate() {
-						if (0..=1).contains(&basis[i]) {
+						if (0..NUM_I).contains(&basis[i]) {
 							*item_j += item_i[j] * self.final_equation.cof[basis[i]];
 						}
 					}
 				}
 				
-				for (i, item) in delta.iter_mut().enumerate().take(2) {
+				for (i, item) in delta.iter_mut().enumerate().take(NUM_I) {
 					*item -= self.final_equation.cof[i];
 				}
 				
 				self.delta_history.push(delta);
 				
 				if self.is_optimal(&delta) {
-					let mut x = [0.0; 5];
+					let mut x = [0.0; NUM_I + NUM_J];
 					for (i, item) in simple_tab.iter().enumerate() {
-						x[basis[i]] = item[5];
+						x[basis[i]] = item[NUM_I + NUM_J];
 					}
 					
-					self.result = Some([x[0], x[1]]);
+					self.result = Some(x);
 					break;
 				}
 				
 				let mut max_min = 0.0;
 				let mut max_min_j = 0;
-				for (j, item) in delta.iter().enumerate().take(5) {
+				for (j, item) in delta.iter().enumerate().take(NUM_I + NUM_J) {
 					match self.final_equation.limit {
 						Limit::Max => {
 							if *item < max_min {
@@ -354,8 +367,8 @@ impl MyApp {
 				let mut min = f64::INFINITY;
 				let mut min_i = None;
 				for (i, item) in simple_tab.iter().enumerate() {
-					if item[max_min_j] > 0.0 && item[5] / item[max_min_j] < min {
-						min = item[5] / item[max_min_j];
+					if item[max_min_j] > 0.0 && item[NUM_I + NUM_J] / item[max_min_j] < min {
+						min = item[NUM_I + NUM_J] / item[max_min_j];
 						min_i = Some(i);
 					}
 				}
@@ -377,7 +390,9 @@ impl eframe::App for MyApp {
 			ui.horizontal(|ui| {
 				add_drag_value(ui, &mut self.final_equation.cof[0], "z = ", &mut changed);
 				add_drag_value(ui, &mut self.final_equation.cof[1], "x1 + ", &mut changed);
-				ui.label("x2 -> ");
+				add_drag_value(ui, &mut self.final_equation.cof[2], "x2 + ", &mut changed);
+				add_drag_value(ui, &mut self.final_equation.cof[3], "x3 + ", &mut changed);
+				ui.label("x4 -> ");
 				ComboBox::from_id_salt(3)
 					.selected_text(self.final_equation.limit.to_string())
 					.width(20.0)
@@ -390,11 +405,13 @@ impl eframe::App for MyApp {
 						};
 					});
 			});
-			for i in 0..3 {
+			for i in 0..NUM_J {
 				ui.horizontal(|ui| {
 					add_drag_value(ui, &mut self.equations[i].cof[0], "", &mut changed);
 					add_drag_value(ui, &mut self.equations[i].cof[1], "x1 + ", &mut changed);
-					ui.label("x2 ");
+					add_drag_value(ui, &mut self.equations[i].cof[2], "x2 + ", &mut changed);
+					add_drag_value(ui, &mut self.equations[i].cof[3], "x3 + ", &mut changed);
+					ui.label("x4 ");
 					ComboBox::from_id_salt(i)
 						.selected_text(self.equations[i].cmp.to_string())
 						.width(15.0)
@@ -406,7 +423,7 @@ impl eframe::App for MyApp {
 							changed = true
 						};
 						});
-					add_drag_value(ui, &mut self.equations[i].cof[2], "", &mut changed);
+					add_drag_value(ui, &mut self.equations[i].cof[4], "", &mut changed);
 				});
 			}
 			
@@ -422,13 +439,19 @@ impl eframe::App for MyApp {
 							.id_salt((i * 2) + 1001)
 							.vscroll(false)
 							.striped(true)
-							.columns(Column::auto().at_least(50.0), 7)
+							.columns(Column::auto().at_least(50.0), NUM_I + NUM_J + 2)
 							.header(20.0, |mut header| {
-								for label in ["", "x1", "x2", "x3", "x4", "x5", "b"] {
+								header.col(|ui| {
+									ui.heading("");
+								});
+								for i in 0..NUM_I + NUM_J {
 									header.col(|ui| {
-										ui.heading(label);
+										ui.heading("x".to_owned() + (i + 1).to_string().as_ref());
 									});
 								}
+								header.col(|ui| {
+									ui.heading("b");
+								});
 							})
 							.body(|mut body| {
 								for (j, items_row) in simple_tab.iter().enumerate() {
@@ -451,13 +474,19 @@ impl eframe::App for MyApp {
 								.id_salt(i * 2 + 1000)
 								.vscroll(false)
 								.striped(true)
-								.columns(Column::auto().at_least(50.0), 7)
+								.columns(Column::auto().at_least(50.0), NUM_I + NUM_J + 2)
 								.header(20.0, |mut header| {
-									for label in ["", "d1", "d2", "d3", "d4", "d5", "db"] {
+									header.col(|ui| {
+										ui.heading("");
+									});
+									for i in 0..NUM_I + NUM_J {
 										header.col(|ui| {
-											ui.heading(label);
+											ui.heading("d".to_owned() + (i + 1).to_string().as_ref());
 										});
 									}
+									header.col(|ui| {
+										ui.heading("db");
+									});
 								})
 								.body(|mut body| {
 									body.row(20.0, |mut row| {
@@ -473,38 +502,54 @@ impl eframe::App for MyApp {
 						ui.add_space(10.0);
 					}
 				});
-				
-			Plot::new("plot")
-				.auto_bounds(false)
-				.x_axis_label("x1")
-				.y_axis_label("x2")
-				.show(ui, |plot| {
-					for i in 0..3 {
-						let x1 = 1000.0;
-						let x2 = (self.equations[i].cof[2] - self.equations[i].cof[0] * x1) / self.equations[i].cof[1];
-						let x1_2 = -1000.0;
-						let x2_2 = (self.equations[i].cof[2] - self.equations[i].cof[0] * x1_2) / self.equations[i].cof[1];
-						plot.add(Line::new(Vec::from([[x1, x2], [x1_2, x2_2]]))
-							.color(Color32::BLUE)
-							.width(2.0)
-						);
+			
+			if let Some(result) = self.result {
+				let mut sum = 0.0;
+				ui.horizontal(|ui| {
+					for (i, item_i) in result.iter().take(NUM_I).enumerate() {
+						ui.label(format!("{:.2}", *item_i));
+						ui.add_space(10.0);
+						sum += *item_i * self.final_equation.cof[i];
 					}
-					
-					if let Some(result) = self.result {
-						plot.add(Points::new([result[0], result[1]]).color(Color32::RED).radius(5.0));
+				});
+				ui.add_space(10.0);
+				ui.label(format!("{:.2}", sum));
+				ui.add_space(10.0);
+			}
+			
+			if NUM_I == 2 {
+				Plot::new("plot")
+					.auto_bounds(false)
+					.x_axis_label("x1")
+					.y_axis_label("x2")
+					.show(ui, |plot| {
+						for i in 0..NUM_J {
+							let x1 = 1000.0;
+							let x2 = (self.equations[i].cof[2] - self.equations[i].cof[0] * x1) / self.equations[i].cof[1];
+							let x1_2 = -1000.0;
+							let x2_2 = (self.equations[i].cof[2] - self.equations[i].cof[0] * x1_2) / self.equations[i].cof[1];
+							plot.add(Line::new(Vec::from([[x1, x2], [x1_2, x2_2]]))
+								.color(Color32::BLUE)
+								.width(2.0)
+							);
+						}
 						
-						let x2_1 = (result[0] - 1000.0) * -self.final_equation.cof[0] / self.final_equation.cof[1];
-						let x2_2 = (result[0] + 1000.0) * -self.final_equation.cof[0] / self.final_equation.cof[1];
-						plot.add(Line::new(Vec::from([
-							[2.0 * result[0] - 1000.0, x2_1 + result[1]],
-							[2.0 * result[0] + 1000.0, x2_2 + result[1]]
-						]))
-							.color(Color32::GREEN)
-							.width(2.0));
-					}
-					
-					plot.add(Polygon::new(self.dots.clone()).fill_color(Color32::from_rgba_premultiplied(80, 80, 0, 100)))
-				})
+						if let Some(result) = self.result {
+							plot.add(Points::new([result[0], result[1]]).color(Color32::RED).radius(5.0));
+							
+							let x2_1 = (result[0] - 1000.0) * -self.final_equation.cof[0] / self.final_equation.cof[1];
+							let x2_2 = (result[0] + 1000.0) * -self.final_equation.cof[0] / self.final_equation.cof[1];
+							plot.add(Line::new(Vec::from([
+								[2.0 * result[0] - 1000.0, x2_1 + result[1]],
+								[2.0 * result[0] + 1000.0, x2_2 + result[1]]
+							]))
+								.color(Color32::GREEN)
+								.width(2.0));
+						}
+						
+						plot.add(Polygon::new(self.dots.clone()).fill_color(Color32::from_rgba_premultiplied(80, 80, 0, 100)))
+					});
+			};
 		});
 	}
 }
